464d9c0eabea0859295ae56438e9b936ef1ba934
Ashar Fuadi <fushar@gmail.com>
2017-08-07 17:00:35 +0700

    Initial spec for sc

---
+++ penyisihan/sc/spec.cpp
@@ -0,0 +1,66 @@
+#include <tcframe/spec.hpp>
+#include <bits/stdc++.h>
+
+using namespace tcframe;
+using namespace std;
+
+class ProblemSpec : public BaseProblemSpec {
+protected:
+    int N;
+    vector<int> P;
+    vector<vector<int>> C;
+
+    int res;
+
+    void InputFormat() {
+        LINE(N);
+        LINES(P, C) % SIZE(N);
+    }
+
+    void OutputFormat() {
+        LINE(res);
+    }
+
+    void Constraints() {
+        CONS(validCompanies());
+    }
+
+    void Subtask1() {
+        CONS(1 <= N && N <= 10);
+        CONS(eachElementBetween(P, 0, 1));
+    }
+
+    void Subtask2() {
+        CONS(1 <= N && N <= 50);
+        CONS(eachElementBetween(P, 0, 50));
+    }
+
+private:
+    bool eachElementBetween(const vector<int>& v, int lo, int hi) {
+        return all_of(v.begin(), v.end(), [lo, hi](int a) {return lo <= a && a <= hi;});
+    }
+
+    bool validCompanies() {
+        for (int i = 0; i < N; i++) {
+            if (P[i] != C[i].size()) return false;
+
+            for (int c : C[i]) {
+                if (c < 1 || c > 50) return false;
+            }
+        }
+        return true;
+    }
+};
+
+class TestSpec : public BaseTestSpec<ProblemSpec> {
+protected:
+    void TestGroup1() {
+        Subtasks({1, 2});
+
+    }
+
+    void TestGroup2() {
+        Subtasks({2});
+
+    }
+};

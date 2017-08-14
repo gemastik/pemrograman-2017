f2b7e7bfb1d119d3a1827fc9ee2d335ef321fdf4
Alham Fikri Aji <afaji321@gmail.com>
2017-08-14 15:14:12 +0000

    add pairing spec

---
+++ penyisihan/pairing/VectorRandomizer.hpp
@@ -0,0 +1,174 @@
+#pragma once
+#include <algorithm>
+#include <stdexcept>
+#include <vector>
+#include <set>
+#include <map>
+#include <string>
+#include <functional>
+#include <cstdio>
+#include <iostream>
+
+using namespace std;
+using std::mt19937;
+using std::uniform_int_distribution;
+
+namespace tcrand {
+
+    template <class T>
+    class VectorRandomizer{
+        int param_length;
+        int param_distinct_elements;
+        int param_min_count;
+        int param_max_count;
+        bool param_is_unique;
+        bool param_range_set;
+        T param_element_min;
+        T param_element_max;
+        function<T(void)> generator;
+        vector<T> param_pool;
+
+        mt19937 r_engine;
+
+        int param_pool_id;
+
+        int nextInt(int minNum, int maxNum) {
+            uniform_int_distribution<int> distr(minNum, maxNum);
+            return distr(r_engine);
+        }
+
+        T next_random_element(){
+            if (param_pool.size() == 0)
+                return generator();
+
+            if (!param_is_unique)
+                return param_pool[nextInt(0, param_pool.size() - 1)];
+
+            return param_pool[param_pool_id++];
+        }
+
+        vector<T> __next(){
+            int fail_attempt = 0;
+            vector<T> result;
+            map<T, int> counts;
+            vector<T> result_set;
+
+            while (fail_attempt < 1000 && result.size() < param_length){
+                //attempt to add a possibly new element:
+                if (result_set.size() < param_distinct_elements || param_distinct_elements == -1){
+                    T element = next_random_element();
+
+                    if (param_range_set && (element < param_element_min || element > param_element_max)){
+                        fail_attempt++;
+                        continue;
+                    }
+                    //if exists before, try again
+                    if ((param_is_unique && counts[element]) || (counts[element] + 1 > param_max_count)){
+                        fail_attempt++;
+                        continue;
+                    }
+
+                    //if adding it makes the array longer, try again
+                    //if exists before, try again
+                    if (result.size() + param_min_count > param_length){
+                        fail_attempt++;
+                        continue;
+                    }
+
+                    //if this element is not in our current result, add this by "param_min_count"
+                    //otherwise, just add one
+                    int add_amount = 1;
+                    if (counts[element] == 0) {
+                        result_set.push_back(element);
+                        add_amount = param_min_count;
+                    }
+
+                    for (int i = 0;i < add_amount; i++) {
+                        result.push_back(element);
+                        counts[element]++;
+                    }
+                }
+
+                //attempt to add an old element:
+                else if (result_set.size() > 0 && param_is_unique == false){
+                    int pick_id = nextInt(0, result_set.size() - 1);
+
+                    T element = result_set[pick_id];
+
+                    counts[element]++;
+                    result.push_back(element);
+
+                    if (counts[element] == param_max_count) {
+                        swap(result_set[pick_id], result_set[result_set.size() - 1] );
+                        result_set.pop_back();
+                    }
+                } else {
+                    fail_attempt++;
+                }
+            }
+            
+            if (fail_attempt == 1000){
+                throw runtime_error("failed to generate vector");
+            }
+
+            random_shuffle(result.begin(), result.end());
+            return result;
+        }
+
+    public:
+        VectorRandomizer(){
+            param_length = 10;
+            param_is_unique = false;
+            param_range_set = false;
+            param_distinct_elements = -1;
+            param_max_count = 9999999;
+            param_min_count = 1;
+        }
+
+        VectorRandomizer& length(int n){
+            param_length = n;
+            return *this;
+        }
+
+        VectorRandomizer& unique(bool u = true){
+            param_is_unique = u;
+            param_distinct_elements = -1;
+            return *this;
+        }
+
+        VectorRandomizer& distinct_elements(int n){
+            param_is_unique = false;
+            param_distinct_elements = n;
+            return *this;
+        }
+
+        VectorRandomizer& element_counts(int min_count, int max_count){
+            param_min_count = min_count;
+            param_max_count = max_count;
+            return *this;
+        }
+
+        VectorRandomizer& element_range(T element_min, T element_max){
+            param_range_set = true;
+            param_element_min = element_min;
+            param_element_max = element_max;
+            return *this;
+        }
+
+        VectorRandomizer& engine(function<T(void)> f){
+            generator = f;
+            return *this;
+        }
+
+        VectorRandomizer& pool(vector<T> p) {
+            param_pool = p;
+            random_shuffle(param_pool.begin(), param_pool.end());
+            return *this;
+        }
+
+        vector<T> next(){
+            param_pool_id = 0;
+            return __next();
+        }
+    };
+}
--- penyisihan/pairing/spec.cpp
+++ penyisihan/pairing/spec.cpp
@@ -1,7 +1,7 @@
 #include <tcframe/spec.hpp>
-#include <bits/stdc++.h>
-
+#include "VectorRandomizer.hpp"
 using namespace tcframe;
+using namespace tcrand;
 using namespace std;
 
 class ProblemSpec : public BaseProblemSpec {
@@ -54,7 +54,122 @@ private:
 };
 
 class TestSpec : public BaseTestSpec<ProblemSpec> {
+
+    VectorRandomizer<int> rand_A, rand_B, rand_C;
+    int C_ans;
+
+    vector<int> P(int st, int ed){
+        vector<int> res;
+        for (int i=st;i<=ed;i++)res.push_back(i);
+        return res;
+    }
+
+    vector<int> killer(int siz, int root, int placement=500){
+        vector<int> res;
+        siz-=101;
+        for (int i=0;i<siz;i++) res.push_back(i * root);
+        for (int i=0;i<100;i++) res.push_back(siz * root + i);
+        res.push_back(placement * root + 1);
+        return res;
+    }
+
+    void construct_C(){
+        vector<int> tA = A;
+        vector<int> tB = B;
+        sort(tA.begin(), tA.end());
+        sort(tB.begin(), tB.end());
+        int NA = unique(tA.begin(), tA.end()) - tA.begin();
+        int NB = unique(tB.begin(), tB.end()) - tB.begin();
+
+        C = vector<int>(M);
+
+        priority_queue<pair<long long, pair<int, int> > > pq;
+        pq.push({-1LL * tA[0] * tB[0] , {0,0}});
+        int curr = 0;
+        long long loc = 0;
+        int la=0,lb=0;
+        long long marked[M];
+        long long latest_mark = 0;
+        int mark_step = 0;
+        int mark_count = 0;
+        memset(marked, -1, sizeof(marked));
+        set<pair<int,int> > done;
+
+        while(curr < C_ans && !pq.empty() && mark_count < M) {
+            pair<int,int> pos = pq.top().second;
+
+            pq.pop();
+            if (pos.first >= NA || pos.second >= NB) continue;
+            long long diff = ((long long)tA[pos.first] * tB[pos.second]) - ((long long)tA[0] * tB[0]);
+            if (diff > 1000000) break;
+
+            loc = ((long long)tA[pos.first] * (long long)tB[pos.second]);
+
+            if (marked[loc % M] == -1) {
+                marked[loc % M] = loc;
+                latest_mark = loc;
+                mark_count++;
+                mark_step = curr;
+                la = pos.first; lb = pos.second;
+            }
+
+            if (!done.count({pos.first + 1, pos.second})) {
+                done.insert({pos.first + 1, pos.second});
+                pq.push({-1LL * tA[pos.first + 1] * tB[pos.second], {pos.first + 1, pos.second}});
+            }
+
+            if (!done.count({pos.first, pos.second + 1})) {
+                done.insert({pos.first, pos.second + 1});
+                pq.push({-1LL * tA[pos.first] * tB[pos.second + 1], {pos.first, pos.second + 1}});
+            }
+
+            curr++;
+        }
+
+        long long diff = ((long long)tA[la] * tB[lb]) - ((long long)tA[0] * tB[0]);
+        long long exp_sol = marked[latest_mark % M];
+        
+        /*
+        cerr<<" == "<<endl;
+        cerr<<"expected solution: "<<exp_sol<<endl;
+        cerr<<"sorted solution indexes "<<la<<", "<<lb<<endl;
+        cerr<<"mark count: "<< mark_count <<endl;  
+        cerr<<"mark step: "<< mark_step <<endl;  
+        cerr<<tA[la]<<" "<<tB[lb]<<" vs "<<tA[0]<<" "<<tB[0]<<endl;
+        */
+        int offset =rnd.nextInt(0, 1000000 - diff - 1);
+        //cerr<<offset<<endl;
+        for (int i=0;i<M;i++){
+            if (i == latest_mark) C[i] = offset;
+            else if (marked[i] == -1) C[i] = rnd.nextInt(max(0, offset - 10000), offset -1);
+            else C[i] =  rnd.nextInt(exp_sol - marked[i] + 1 , 1000000 - offset) + offset;
+        }
+    }    
+
+
 protected:
+    void BeforeTestCase() {
+        C_ans = -1;
+        A.clear();
+        B.clear();
+        C.clear();
+        rand_A = VectorRandomizer<int>();
+        rand_B = VectorRandomizer<int>();
+        rand_C = VectorRandomizer<int>();
+    }
+
+    void AfterTestCase() {
+        rand_A.engine( [&] {return rnd.nextInt(0,1000000);} );
+        rand_B.engine( [&] {return rnd.nextInt(0,1000000);} );
+        rand_C.engine( [&] {return rnd.nextInt(0,1000000);} );
+
+        if (A.empty()) A = rand_A.length(N).next();
+        if (B.empty()) B = rand_B.length(N).next();
+        if (C_ans != -1) construct_C();
+        if (C.empty()) C = rand_C.length(M).next();
+    }
+
+
     void SampleTestCase1() {
         Subtasks({1, 2});
         Input({
@@ -81,13 +196,84 @@ protected:
         });
     }
 
+
+    void antiMarshall(int _M) {
+        CASE(N = 25000, M = (_M/2)*2, rand_A.pool(killer(N, 2, 900)).unique(), rand_B.pool(killer(N, 2, 900)).unique(), C_ans = 3000000); 
+        CASE(N = 30000, M = (_M/6)*6, rand_A.pool(killer(N, 2, 6000)).unique(), rand_B.pool(killer(N, 3, 3)).unique(), C_ans = 3000000); 
+        CASE(N = 30000, M = (_M/6)*6, rand_A.pool(killer(N, 3, 20)).unique(), rand_B.pool(killer(N, 2, 3000)).unique(), C_ans = 1000000); 
+        CASE(N = 50000, M = (_M/3)*3, rand_A.pool(killer(N, 3, 123)).unique(), rand_B.pool(killer(N, 3, 321)).unique(), C_ans = 5000000); 
+        CASE(N = 60000, M = (_M/2)*2, rand_A.pool(killer(N, 2, 50000)).unique(), rand_B.pool(killer(N, 2, 2)).unique(), C_ans = 5000000);  
+
+    }
+
+    void killerCases(int _M) {
+        CASE(N = 25000, M = (_M/2)*2 /*genap*/, rand_A.pool(killer(N, 2)).unique(), rand_B.pool(killer(N, 2)).unique(), C_ans = 3000000); //kalo ngeceknya cuman sampe index 1000
+        CASE(N = 100000, M = _M, rand_A.pool(P(3000,1000000)).unique(), rand_B.pool(P(3000,1000000)).unique(), C_ans = 3000000); //kalo ngeceknya cuman sampe nilai A*B <= 1jt
+        CASE(N = 100000, M = _M, rand_A.distinct_elements(10000), rand_B.distinct_elements(10000), C_ans = 12345); //kalo lupa di-unique
+        CASE(N = 100000, M = _M, rand_A.pool(P(1,1000000)).unique(), rand_B.pool(P(0,99999)).unique(), C_ans = 1000000); //ada 0-nya
+        CASE(N = 100000, M = _M, rand_A.pool(P(10000,200000)).unique(), rand_B.pool(P(10000,200000)).unique(), C_ans = 777777); 
+    }
+
+    void randomCases(int _M) {
+        CASE(N = rnd.nextInt(10000, 100000), M = rnd.nextInt(500, _M), C_ans = rnd.nextInt(5, 100));
+        CASE(N = rnd.nextInt(50000, 100000), M = rnd.nextInt(_M - 500, _M), C_ans = rnd.nextInt(5000, 100000), rand_A.pool(P(100,1000000)) , rand_B.pool(P(100, 1000000)));
+        CASE(N = rnd.nextInt(50000, 100000), M = rnd.nextInt(_M - 500, _M));
+        CASE(N = 100000, M = _M);
+        CASE(N = 100000, M = _M, rand_A.pool(P(100,1000000)) , rand_B.pool(P(100, 1000000)) );
+
+    }
+
     void TestGroup1() {
         Subtasks({1, 2});
-
+        CASE(N = 1, M = 1, A = {1000000}, B = {1000000}, C = {1000000}); //edge case 1
+        CASE(N = 100000, M = 1); //edge case 2
+        CASE(N = 1, M = 1); //edge case 3
+        CASE(rand_A.distinct_elements(1), rand_B.distinct_elements(1), N = 100000, M = 1000); //edge case 4
+        CASE(rand_A.pool(P(0,0)), rand_B.pool(P(0,0)), N = 100000, M = 1000); //edge case 5
     }
 
     void TestGroup2() {
-        Subtasks({2});
-
+        Subtasks({1, 2});
+        CASE(N = 50, M = 1000); //small random
+        CASE(N = 100000, M = 997); //big random
+        CASE(rand_A.distinct_elements(300), rand_B.distinct_elements(300), N = 100000, M = 997); //mepet2
+        CASE(rand_A.distinct_elements(10), rand_B.distinct_elements(10), N = 88888, M = 888);
+        CASE(rand_A.unique(), rand_B.unique(), N = 999, M = 999);
     }
+
+    void TestGroup3() {
+        Subtasks({1, 2});
+        killerCases(997);
+    }
+
+    void TestGroup4() {
+        Subtasks({1, 2});
+        randomCases(1000);
+    }
+
+    void TestGroup5() {
+        Subtasks({1, 2});
+        antiMarshall(900);
+    }
+
+
+    void TestGroup6() {
+        Subtasks({2});
+        killerCases(98047);
+    }
+
+    void TestGroup7() {
+        Subtasks({2});
+        randomCases(100000);
+    }
+
+    void TestGroup8() {
+        Subtasks({2});
+        antiMarshall(99999);
+    }
+
+
+    
+
+
 };

d13c80271aeb7bc6c8cd0e108b1b586ec14e5ed2
William Gozali <will.gozali@gmail.com>
2017-10-09 23:23:26 +0700

    Fix constraint (#62)

--- final/bitwise/spec.cpp
+++ final/bitwise/spec.cpp
@@ -4,6 +4,7 @@ using namespace tcframe;
 using namespace std;
 
 
+int BIN_MILLION = (1<<20)-1;
 int MAX_INT = numeric_limits<int>::max();
 
 class ProblemSpec : public BaseProblemSpec {
@@ -41,22 +42,26 @@ protected:
         CONS(1 <= T && T <= 20);
     }
 
-    void Constraints() {
-        CONS(0 <= A && A <= MAX_INT);
-        CONS(0 <= B && B <= MAX_INT);
-        CONS(0 <= C && C <= MAX_INT);
-    }
-
     void Subtask1() {
         CONS(N == 2);
+        CONS(0 <= A && A <= BIN_MILLION);
+        CONS(0 <= B && B <= BIN_MILLION);
+        CONS(0 <= C && C <= BIN_MILLION);
     }
 
     void Subtask2() {
         CONS(1 <= N && N <= 20000);
+        CONS(0 <= A && A <= MAX_INT);
+        CONS(0 <= B && B <= MAX_INT);
+        CONS(0 <= C && C <= MAX_INT);
     }
 };
 
 class TestSpec : public BaseTestSpec<ProblemSpec> {
+    int smallRand(){
+        return rnd.nextInt(0, BIN_MILLION);
+    }
+
     int rand(){
         return rnd.nextInt(0, MAX_INT);
     }
@@ -69,11 +74,11 @@ protected:
             "2 8 14 6"
         });
         // impossible
-        Subtasks({1, 2}); 
+        Subtasks({1, 2});
         Input({
             "2 8 14 4"
         });
-        Subtasks({2}); 
+        Subtasks({2});
         Input({
             "3 8 14 4"
         });
@@ -93,10 +98,10 @@ protected:
         CASE(N = 2, A = 0, B = 145661, C = 145661);
         CASE(N = 2, A = 0, B = 145661, C = 145660);
 
-        CASE(N = 2, A = MAX_INT, B = MAX_INT, C = MAX_INT);
-        CASE(N = 2, A = MAX_INT, B = 0, C = 0);
-        CASE(N = 2, A = 0, B = MAX_INT, C = 0);
-        CASE(N = 2, A = 0, B = 0, C = MAX_INT);
+        CASE(N = 2, A = BIN_MILLION, B = BIN_MILLION, C = BIN_MILLION);
+        CASE(N = 2, A = BIN_MILLION, B = 0, C = 0);
+        CASE(N = 2, A = 0, B = BIN_MILLION, C = 0);
+        CASE(N = 2, A = 0, B = 0, C = BIN_MILLION);
         /*
         p q -> A B C
         0 0 -> 0 0 0
@@ -117,11 +122,11 @@ protected:
     void TestGroup2() {
         Subtasks({1, 2});
         for (int i = 0; i < 4; i++){
-            CASE(N = 2, A = rand(), B = rand(), C = rand());
-            CASE(N = 2, A = rand(), B = rand(), C = rand());
-            CASE(N = 2, A = rand(), B = rand(), C = A ^ B);
-            CASE(N = 2, A = rand(), C = rand(), B = A ^ C);
-            CASE(N = 2, B = rand(), C = rand(), C = B ^ C);
+            CASE(N = 2, A = smallRand(), B = smallRand(), C = smallRand());
+            CASE(N = 2, A = smallRand(), B = smallRand(), C = smallRand());
+            CASE(N = 2, A = smallRand(), B = smallRand(), C = A ^ B);
+            CASE(N = 2, A = smallRand(), C = smallRand(), B = A ^ C);
+            CASE(N = 2, B = smallRand(), C = smallRand(), A = B ^ C);
         }
     }
 
@@ -130,7 +135,7 @@ protected:
         //zonk
         CASE(N = 1, A = 321, B = 321, C = 321);
         CASE(N = 1, A = 321, B = 321, C = 53234);
-        CASE(N = 1, A = 321, B = 5555, C = 321);        
+        CASE(N = 1, A = 321, B = 5555, C = 321);
         CASE(N = 1, A = 43100, B = 321, C = 321);
 
         // N = 3
@@ -174,6 +179,15 @@ protected:
             CASE(N = (rnd.nextInt(20000, 39000)/2) + i, A = rand(), C = rand(), B = A ^ C);
             CASE(N = (rnd.nextInt(20000, 39000)/2) + i, B = rand(), C = rand(), C = B ^ C);
         }
+    }
+
+     void TestGroup5() {
+        Subtasks({2});
+
+        CASE(N = 2, A = MAX_INT, B = MAX_INT, C = MAX_INT);
+        CASE(N = 2, A = MAX_INT, B = 0, C = 0);
+        CASE(N = 2, A = 0, B = MAX_INT, C = 0);
+        CASE(N = 2, A = 0, B = 0, C = MAX_INT);
     }
 };
 

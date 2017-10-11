6c7f6c2059d7bcf7f53891b897eb302b7b57d86f
Pusaka Kaleb Setyabudi <sokokaleb@users.noreply.github.com>
2017-10-11 22:43:19 +0700

    Bis DDAK: Slight revision on spec (#77)
    * Bis DDAK: Slight revision on spec
    
    * M can be 0 lah
    
    * Missed checking for LOOP_COST
    
    * Remove files keikut
    
    * LOOP_COST.size() should be == N

--- final/bis-ddak/spec.cpp
+++ final/bis-ddak/spec.cpp
@@ -8,6 +8,7 @@ template<typename T>
 inline bool inRange(const T& z, const T& lb, const T& ub) { return lb <= z && z <= ub; }
 
 const int MAX_N = 500;
+const int SMALL_MAX_N = 12;
 const int MAX_M = 50000;
 const int MAX_EDGE_COST = 2000000000 / MAX_N;
 
@@ -23,15 +24,23 @@ protected:
     int ANS_COST, ANS_NUM_CIRCUIT;
     vector<vi> ANS_CIRCUITS;
     vi ANS_CIRCUITS_SIZE;
+    vi LOOP_COST;
+
+    string MUSTAHIL;
 
     void InputFormat() {
         LINE(N, M);
+        LINE(LOOP_COST % SIZE(N));
         LINES(U, V, W) % SIZE(M);
     }
 
-    void OutputFormat() {
+    void OutputFormat1() {
         LINE(ANS_COST, ANS_NUM_CIRCUIT);
         LINES(ANS_CIRCUITS_SIZE, ANS_CIRCUITS) % SIZE(ANS_NUM_CIRCUIT);
+    }
+
+    void OutputFormat2() {
+        LINE(MUSTAHIL);
     }
 
     void MultipleTestCasesConfig() {
@@ -44,7 +53,9 @@ protected:
 
     void Constraints() {
         CONS(inRange(N, 1, MAX_N));
-        CONS(inRange(M, 1, MAX_M));
+        CONS(inRange(M, 0, MAX_M));
+        CONS(LOOP_COST.size() == N);
+        CONS(all_of(LOOP_COST.begin(), LOOP_COST.end(), [&](int w) { return inRange(w, 1, MAX_EDGE_COST); }));
         CONS(all_of(W.begin(), W.end(), [&](int w) { return inRange(w, 1, MAX_EDGE_COST); }));
         CONS(all_of(U.begin(), U.end(), [&](int u) { return inRange(u, 1, N); }));
         CONS(all_of(V.begin(), V.end(), [&](int v) { return inRange(v, 1, N); }));

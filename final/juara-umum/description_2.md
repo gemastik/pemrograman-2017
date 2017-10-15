d128f0e9e8a6b799693e32c848a887e51bd9c504
Ashar Fuadi <fushar@gmail.com>
2017-10-15 13:47:44 +0700

    Deskripsi juara umum (#85)

---
+++ final/juara-umum/description_2.md
@@ -0,0 +1,95 @@
+# Juara Umum (Sulit)
+
+Tahun ini, Gemastik terdiri atas N cabang lomba, yang dinomori dari 1 hingga N. Terdapat M universitas sebagai peserta Gemastik, yang dinomori dari 1 hingga M. Setiap universitas mengirimkan tepat satu tim untuk setiap cabang lomba.
+
+Untuk setiap cabang lomba, akan diambil tiga tim berbeda sebagai pemenang, yang berturut-turut akan memperoleh medali emas, perak, dan perunggu. Setelah semua cabang lomba dipertandingkan, peringkat seluruh universitas akan diurutkan berdasarkan perolehan medali. Universitas A akan meraih peringkat yang lebih baik daripada universitas B, apabila:
+
+- perolehan emas A > perolehan emas B, atau
+- perolehan emas A = perolehan emas B dan perolehan perak A > perolehan perak B, atau
+- perolehan emas A = perolehan emas B, perolehan perak A = perolehan perak B, dan perolehan perunggu A > perolehan perunggu B.
+
+Akhirnya, universitas yang memiliki peringkat terbaik akan mendapatkan gelar juara umum.
+
+Perhatikan bahwa menurut sistem pemeringkatan di atas, mungkin saja terdapat lebih dari satu juara umum. Mungkinkah terdapat tepat K universitas sebagai juara umum?
+
+### Format Masukan
+
+Baris pertama berisi sebuah bilangan bulat T yang menyatakan banyaknya kasus uji. Baris-baris berikutnya berisi T kasus uji, yang masing-masing diberikan dalam format berikut ini:
+
+```
+N M K
+```
+
+### Format Keluaran
+
+Untuk setiap kasus uji:
+
+Jika tidak mungkin terdapat tepat K universitas sebagai juara umum, keluarkan:
+
+```
+mustahil
+```
+
+Jika mungkin, keluarkan:
+
+```
+mungkin
+G[1] S[1] B[1]
+G[2] S[2] B[2]
+.
+.
+G[N] S[N] B[N]
+```
+
+dengan G[i], S[i], B[i] masing-masing merupakan nomor universitas yang mendapatkan medali emas, perak, dan perunggu pada cabang lomba ke-i. Jika terdapat lebih dari satu kemungkinan, keluarkan yang mana saja.
+
+### Contoh Masukan
+
+```
+4
+3 3 2
+5 10 2
+4 3 3
+8 6 3
+```
+
+### (Salah Satu) Contoh Keluaran
+
+```
+mustahil
+mungkin
+1 9 10
+1 9 10
+2 6 7
+2 6 7
+5 9 10
+mustahil
+mungkin
+1 2 6
+1 2 6
+2 3 6
+2 3 6
+3 1 6
+3 1 6
+4 5 6
+5 4 6
+```
+
+### Penjelasan
+
+Untuk contoh kedua, universitas 1 dan universitas 2 menjadi juara umum bersama, karena masing-masing memperoleh 2 medali emas.
+
+Untuk contoh keempat, universitas 1, 2, dan 3 menjadi juara umum bersama, karena masing-masing memperoleh 2 medali emas dan 2 medali perak.
+
+### Batasan
+
+- 1 ≤ T ≤ 10
+- 1 ≤ N ≤ 50.000
+- 3 ≤ M ≤ 50.000
+- K ≤ N
+
+### Batasan Khusus Versi Sulit
+
+**Soal ini adalah soal versi sulit**. Batasan khusus untuk soal versi sulit:
+
+- 1 ≤ K ≤ M
--- final/juara-umum/spec.cpp
+++ final/juara-umum/spec.cpp
@@ -61,45 +61,47 @@ protected:
         Subtasks({1, 2});
         Input({
             "3 3 2"
-        }); // Tidak mungkin
+        });
+        Output({"mustahil"});
     }
     void SampleTestCase2() {
        Subtasks({1, 2});
         Input({
             "5 10 2"
         });
-        /*
-            Mungkin
-            1 9 10
-            1 9 10
-            2 6 7
-            2 6 7
-            5 9 10
-        */
+        Output({
+            "mungkin",
+            "1 9 10",
+            "1 9 10",
+            "2 6 7",
+            "2 6 7",
+            "5 9 10"
+        });
     }
-    void SampleTestCase3(){ 
-        Subtasks({2}); 
+    void SampleTestCase3(){
+        Subtasks({2});
         Input({
             "4 3 3"
-        }); // Tidak Mungkin
+        });
+        Output({"mustahil"});
     }
-    
+
     void SampleTestCase4() {
-        Subtasks({2}); 
+        Subtasks({2});
         Input({
             "8 6 3"
-        }); 
-        /*
-            Mungkin
-            1 2 6
-            1 2 6
-            2 3 6
-            2 3 6
-            3 1 6
-            3 1 6
-            4 5 6
-            5 4 6
-        */
+        });
+        Output({
+            "mungkin",
+            "1 2 6",
+            "1 2 6",
+            "2 3 6",
+            "2 3 6",
+            "3 1 6",
+            "3 1 6",
+            "4 5 6",
+            "5 4 6"
+        });
     }
 
     void TestGroup1() {
@@ -127,21 +129,21 @@ protected:
         CASE(N = 1500, M = 500, K = 500); // possible
         CASE(N = 1503, M = 501, K = 500); // impossible
         CASE(N = 1502, M = 501, K = 500); // possible
-        CASE(N = 1562, M = 521, K = 500); // possible    
+        CASE(N = 1562, M = 521, K = 500); // possible
     }
-    
+
     void TestGroup3() {
         Subtasks({2});
         //gede2 semua
-        CASE(N = 20000, M = 20000, K = 19997); 
-        CASE(N = 19865, M = 20000, K = 1337); 
-        CASE(N = 19777, M = 20000, K = 999); 
-        CASE(N = 18900, M = 20000, K = 12345); 
-        CASE(N = 15000, M = 20000, K = 200); 
+        CASE(N = 20000, M = 20000, K = 19997);
+        CASE(N = 19865, M = 20000, K = 1337);
+        CASE(N = 19777, M = 20000, K = 999);
+        CASE(N = 18900, M = 20000, K = 12345);
+        CASE(N = 15000, M = 20000, K = 200);
         CASE(N = 20000, M = 3, K = 3);
         CASE(N = 19000, M = 20, K = 19);
-        CASE(N = 18766, M = 50, K = 45); 
-        CASE(N = 19997, M = 4, K = 3); 
+        CASE(N = 18766, M = 50, K = 45);
+        CASE(N = 19997, M = 4, K = 3);
         CASE(N = 19993, M = 7, K = 6);
     }
 };

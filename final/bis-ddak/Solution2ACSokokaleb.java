b0bc2575efce02a726d122d6e9f378c9a48f34b4
Pusaka Kaleb Setyabudi <sokokaleb@users.noreply.github.com>
2017-10-22 12:24:01 +0700

    bis-ddak: Fix mabocc on tracing the answer (#137)

--- final/bis-ddak/Solution2ACSokokaleb.java
+++ final/bis-ddak/Solution2ACSokokaleb.java
@@ -125,11 +125,7 @@ public class Solution2ACSokokaleb {
         visited.clear();
 
         for (int i = 0; i < N; ++i) {
-            for (int j = 0; j < N; ++j) {
-                if (lx[i] + ly[j] == arr[i][j]) {
-                    to[i] = j;
-                }
-            }
+            to[prev[i]] = i;
         }
 
         for (int i = 0; i < N; ++i) {
